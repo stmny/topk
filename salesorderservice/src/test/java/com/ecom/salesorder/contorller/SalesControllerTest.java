4703408660e34797ae618b5f03feaddae609e4a5
stmny <yongniu@gmail.com>
2018-03-29 13:44:36 -0700

    re organize code

--- salesorderservice/src/test/java/com/ecom/salesorder/contorller/SalesControllerTest.java
+++ salesorderservice/src/test/java/com/ecom/salesorder/contorller/SalesControllerTest.java
@@ -6,6 +6,8 @@ import com.ecom.salesorder.model.SalesOrderModel;
 import com.ecom.salesorder.model.SalesOrderReqest;
 import com.ecom.salesorder.model.SalesOrderResponse;
 import com.ecom.salesorder.service.SalesOrderService;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDateTime;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.testng.annotations.BeforeMethod;
@@ -13,6 +15,8 @@ import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.powermock.api.mockito.PowerMockito.when;
@@ -30,31 +34,34 @@ public class SalesControllerTest {
         MockitoAnnotations.initMocks(this);
         salesOrderController = new SalesOrderController(salesOrderService);
         salesOrder = new SalesOrder();
-        salesOrder.setCount(10);
-        salesOrder.setCreated_at("2018-03-05");
-        salesOrder.setUpdated_at("2018-03-06");
-        salesOrder.setCustomer_id("test_customer");
-        salesOrder.setProduct("testProduct");
+        salesOrder.setCount(new Random().nextInt(1000));
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeZone tz = DateTimeZone.getDefault();
+        LocalDateTime createday = date.minusDays(60);
+        salesOrder.setCreated_at(createday.toDateTime(tz).toString());
+        salesOrder.setUpdated_at(date.toDateTime(tz).toString());
+        salesOrder.setCustomer_id("customer_"+ UUID.randomUUID());
+        salesOrder.setProduct("Product_"+ new Random().nextInt(10));
 
-        salesOrderModel = new SalesOrderModel("test_customer", "2018-03-06",
-                "testProduct","2018-03-05", 10);
+        salesOrderModel = new SalesOrderModel(salesOrder.getCustomer_id(), salesOrder.getUpdated_at(),
+                salesOrder.getProduct(),salesOrder.getCreated_at(), salesOrder.getCount());
 
     }
     @Test
     public void getSalesOrderByCustomerIdTest() {
         List<SalesOrder> salesOrders = new ArrayList<>();
         salesOrders.add(salesOrder);
-        when(salesOrderService.getSalesOrdersByCustomerId("test_customer"))
+        when(salesOrderService.getSalesOrdersByCustomerId(salesOrder.getCustomer_id()))
                 .thenReturn(salesOrders);
-        SalesOrderResponse response = salesOrderController.getSalesOrderByCustomerId("test_customer");
-        assertThat(response.getSalesOrderModelList().get(0).getCount() == 10);
-        assertThat(response.getSalesOrderModelList().get(0).getCustomer_id().equals("test_customer"));
+        SalesOrderResponse response = salesOrderController.getSalesOrderByCustomerId(salesOrder.getCustomer_id());
+        assertThat(response.getSalesOrderModelList().get(0).getCount() == salesOrder.getCount());
+        assertThat(response.getSalesOrderModelList().get(0).getCustomer_id().equals(salesOrder.getCustomer_id()));
     }
     @Test
     public void saveSalesOrderTest() {
         SalesOrderReqest salesOrderReqest = new SalesOrderReqest();
         salesOrderReqest.setSalesOrderModel(salesOrderModel);
         salesOrderController.saveSalesOrder(salesOrderReqest);
-        assertThat(salesOrder.getCount() == 10);
+        assertThat(salesOrder.getCount() == salesOrderModel.getCount());
     }
 }
